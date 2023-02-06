c116b73ada3876989c8eca122c5cf38d693dceec
Anton <antonukin@icloud.com>
2023-02-06 16:52:02 +0300

    task_3 added

---
+++ task_3/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void RandomizeArray(int[] array)
+{
+    Random rnd = new Random();
+    for(int index = 0; index < array.Length; index++)
+    array[index] = rnd.Next(1, 10);
+}
+
+void PrintArray(int[] arr)
+{
+    for(int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+}
+
+int[] newArray = new int[8];
+RandomizeArray(newArray);
+PrintArray(newArray);
